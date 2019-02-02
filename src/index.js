0633beb29238d48a9a702e54831640a35ff9d947
Erislandio Soares <erislandiosoares21@gmail.com>
2019-02-02 13:31:32 -0200

    Adiçaõ do mongoose

--- src/index.js
+++ src/index.js
@@ -6,10 +6,6 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
-    res.send({
-        hello: 'Olá'
-    })
-})
+
 
 app.listen(PORT, () => console.log(`Servidor online na porta ${PORT}...`))
