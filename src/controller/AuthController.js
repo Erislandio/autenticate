8fc769a39159ce00c1b6222125ad06bb3713bda1
Erislandio Soares <erislandiosoares21@gmail.com>
2019-02-02 15:19:13 -0200

    Adição da rota de autenticação

--- src/controller/AuthController.js
+++ src/controller/AuthController.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+const jst = require('jsonwebtoken')
 
 const router = express.Router()
 
@@ -20,6 +22,20 @@ router.post('/register', async (req, res) => {
     } catch (error) {
         res.status(400).send(error)
     }
+})
+
+
+router.post('/authenticate', async (req, res) => {
+    const { email, password } = req.body
+    const user = await User.findOne({ email }).select('+password')
+    if (!user)
+        return res.status(400).send({ Erro: 'Usuário não encontrado' })
+
+    if (!await bcrypt.compare(password, user.password))
+        return res.status(400).send({ Erro: 'Senha inválida' })
+
+    res.send({ user })
+
 })
 
 module.exports = app => app.use('/auth', router)
